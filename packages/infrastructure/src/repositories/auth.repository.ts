3a91c6c883c380e9596cd87cd498814bbd11910a
Victor Huang <huang.victor.mail@gmail.com>
2025-02-06 22:48:07 +0800

    feat : setup infrastructure package for auth

---
+++ packages/infrastructure/src/repositories/auth.repository.ts
@@ -0,0 +1,44 @@
+import type { ApolloClient, NormalizedCacheObject } from "@apollo/client"
+import type { AuthRepository, AuthCredentials, AuthResult, User } from "@genz-deezer/core"
+
+import { GET_ME, LOGIN, REGISTER, UPDATE_PROFILE } from "@/api/graphql/auth.queries"
+
+
+export class ApolloAuthRepository implements AuthRepository {
+  constructor(private readonly client: ApolloClient<NormalizedCacheObject>) {}
+
+  async login(credentials: AuthCredentials): Promise<AuthResult> {
+    const { data } = await this.client.mutate({
+      mutation: LOGIN,
+      variables: credentials
+    })
+
+    return data.login
+  }
+
+  async register(credentials: AuthCredentials): Promise<AuthResult> {
+    const { data } = await this.client.mutate({
+      mutation: REGISTER,
+      variables: credentials
+    })
+
+    return data.register
+  }
+
+  async getCurrentUser(): Promise<User> {
+    const { data } = await this.client.query({
+      query: GET_ME
+    })
+
+    return data.me
+  }
+
+  async updateProfile(email: string): Promise<User> {
+    const { data } = await this.client.mutate({
+      mutation: UPDATE_PROFILE,
+      variables: { email }
+    })
+
+    return data.updateProfile
+  }
+}
