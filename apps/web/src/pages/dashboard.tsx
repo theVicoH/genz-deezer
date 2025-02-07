8b8a6e53b9e77afe87612bfc2c1ce3503f70adf3
Victor Huang <huang.victor.mail@gmail.com>
2025-02-08 06:51:57 +0800

    feat : render new track page

--- apps/web/src/pages/dashboard.tsx
+++ apps/web/src/pages/dashboard.tsx
@@ -1,15 +1,15 @@
+import DeezerPlayer from "@/components/organisms/deezer-player"
 import UserNav from "@/components/organisms/user-nav"
-import MusicRandomizer from "./temp"
 
 const Dashboard = () => {
   return (
-    <div className="flex min-h-screen flex-col w-full">
+    <div className="flex min-h-screen w-full flex-col">
       <div className="flex items-center justify-between px-8 py-4">
         <h1 className="text-3xl font-bold">Tableau de bord</h1>
         <UserNav />
       </div>
-      <div className="flex flex-1 items-center justify-center w-full">
-        <MusicRandomizer />
+      <div className="flex w-full flex-1 items-center justify-center">
+        <DeezerPlayer />
       </div>
     </div>
   )
